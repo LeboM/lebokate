a51d3af35484a9619f54f00cfa2a6dff22374c88
Lebo M. Modimola <leboo@ymail.com>
2013-05-10 05:43:24 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lebokate::Application.config.secret_token = 'a13674c24b455d7446722adb22af2aa1dccea136deca4d3122827184b04da1bc87829ce9d00db782dda40818ac0ba64cb7596cde2e93b453355b6c621ac23217'
